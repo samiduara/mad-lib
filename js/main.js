60ca223518ed830c93651b4dfcfc60c05beb433f
sam <samgathon@hotmail.com>
2019-08-13 10:09:06 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function madLibs() {
+
+    var mystory= document.getElementById("mystory");
+    var adjective = document.getElementById("adjective").value;
+    var title = document.getElementById("title").value;
+    var noun =  document.getElementById("noun").value;
+    var adjective = document.getElementById("adjective").value;
+    var noun = document.getElementById("noun").value;
+    mystory.innerHTML ="NFL: quarter Back " + adjective + ". The the worst quarter back in the Nfl history " + title + ". Its been over 20 years and cowboys fans are still waiting for him to redeem the team " + adjective + " Rumor has it that , Dak preskot " + noun + " will win a superbowl " + noun + " but that was a lie,If you have a car containing a Cowboys running back, a Cowboys linebacker, and a Dallas Cowboys defensive back, who is driving the car?"  
+}
