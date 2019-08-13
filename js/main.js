3ee440437831a926c802d983439d1161f0cd8cf6
sam <samgathon@hotmail.com>
2019-08-13 13:33:37 -0500

    updated js

--- js/main.js
+++ js/main.js
@@ -6,5 +6,5 @@ function madLibs() {
     var noun =  document.getElementById("noun").value;
     var adjective = document.getElementById("adjective").value;
     var noun = document.getElementById("noun").value;
-    mystory.innerHTML ="NFL: quarter Back " + thing + ". The the worst quarter back in the Nfl history " + title + ". Its been over 20 years and cowboys fans are still waiting for him to redeem the team " + adjective + " Rumor has it that , Dak preskot " + noun + " will win a superbowl " + noun + " but that was a lie,If you have a car containing a Cowboys running back, a Cowboys linebacker, and a Dallas Cowboys defensive back, who is driving the car?"  
+    mystory.innerHTML ="NFL: Why did Tony Romo cross " + thing + ".To get to the hospital on the other side!  What does a Dallas Cowboys fan and a bottle of beer have in common? " + title + ".They’re both empty from the neck up.What does a Dallas Cowboys fan and a bottle of beer have in common?" + adjective + " They’re both empty from the neck up. " + noun + " many Dallas Cowboys players claiming they have the Swine Flu " + noun + " will win a superbowl "   
 }
